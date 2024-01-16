c28ed9884aa47e299ad71997d560d8fece3bfc8f
snehavardhan <snehavardhank@gmail.com>
2024-01-16 23:15:54 +0530

    first program, comments

---
+++ 3.1FirstSteps/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){  
+    std::cout << "Hello world" << std::endl;
+    std::cout << "number 1" ;
+    std::cout << "number 2" << std::endl;
+    std::cout << "number 3";
+     return 0;
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cpp
+
+main - starting point of the program
+std:: endl - nextline character
+
+
+Comments :
+  "//" -  "//one line comment"
+  "/*  
+       */"
